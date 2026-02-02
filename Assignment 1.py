8024b613053de8156b522d4286ca55d8713620b4
Manthan Vala <valamanthan94@gmail.com>
2026-02-02 21:53:39 +0530

    Add files via upload
    Assignment 1

---
+++ Assignment 1.py
@@ -0,0 +1,17 @@
+# Task 1:
+num1 = float(input("Enter your first number: "))
+num2 = float(input("Enter your second number: "))
+add = num1 + num2
+sub = num1 - num2
+multi = num1 * num2
+divi = num1 / num2
+print("Addtion of given number: ", round(add, 2))
+print("Subtraction of given number: ", round(sub, 2))
+print("Multiplication of given number: ", round(multi, 2))
+print("Division of given number: ", round(divi, 2))
+
+# Task 2:
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+name = first_name + " " + last_name + "!"
+print("Hello,", name, "Welcome to Python Program.")
